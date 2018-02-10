5b6ecd47fc6fb17d1354fd65c19f7856ce0ff842
Diego Vela <diego@vida.com>
2018-02-10 11:25:47 -0800

    First Commit

--- RxExample/ViewController.swift
+++ RxExample/ViewController.swift
@@ -5,11 +5,16 @@
 //  Created by Diego Vela on 2/10/18.
 //  Copyright © 2018 Diego Vela. All rights reserved.
 //
-
+import RxSwift
+import RxCocoa
+import RxBlocking
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
