3f56602550fc1bd899aed30887466be02e85b3c6
Diego Vela <diego@vida.com>
2018-02-10 11:01:52 -0800

    Initial Commit

---
+++ RxExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxExample
+//
+//  Created by Diego Vela on 2/10/18.
+//  Copyright © 2018 Diego Vela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
