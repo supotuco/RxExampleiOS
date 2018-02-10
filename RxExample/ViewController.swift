cb6c0edfd2de4d1e846c73cec6efb1a7129121c8
Diego Vela <diego@vida.com>
2018-02-10 11:46:16 -0800

    Simple Example

--- RxExample/ViewController.swift
+++ RxExample/ViewController.swift
@@ -14,10 +14,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var myButton: UIButton!
-    
+
+    private var disposable: Disposable? = nil
+
+    private static let colors: [UIColor] = [.yellow, .orange, .blue]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        disposable = myTextField.rx.text
+            .map { $0 ?? "" }
+            .map { $0.count }
+            .map { $0 % ViewController.colors.count }
+            .map { ViewController.colors[$0] }
+            .observeOn(MainScheduler.instance)
+            .subscribe({ color in self.myButton.backgroundColor = color.element! } )
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        disposable?.dispose()
     }
 
     override func didReceiveMemoryWarning() {
